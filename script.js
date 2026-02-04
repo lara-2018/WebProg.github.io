7df27edcbab87292b087403f3ae3cb4a731817ca
lara-2018 <euperle@aol.de>
2026-02-04 10:37:06 +0100

    Aktuellster Stand aller Dateien
    04-02-2026

---
+++ script.js
@@ -0,0 +1,55 @@
+let Zustand = "hide"
+let PwInput = document.getElementById("passwort");
+ let hide = document.querySelector("#hidePassword");
+ let unhide = document.querySelector("#showPassword");
+
+hide.addEventListener("click", () => {
+  hide.style.display = "none";
+  unhide.style.display = "block";
+  PwInput.type = "text"; //Inhalt zeigen
+})
+
+unhide.addEventListener("click", () => {
+  unhide.style.display = "none";
+  hide.style.display = "block";
+  PwInput.type = "password"; //Inhalt verbergen
+})
+
+let MailInput = document.getElementById("E-Mail");
+let Login = document.getElementById("LoginButton");
+
+Login.addEventListener("click", () => {
+  if (MailInput.value === "" || PwInput.value === "") { //wenn ein Feld oder beide Felder leer sind
+    alert("Bitte fülle alle Felder aus");
+  } else {
+    alert("Du hast dich erfolreich eingeloggt");
+  }
+})
+
+ let $pwInput = $("#passwort");
+  let $hide = $("#hidePassword");
+  let $unhide = $("#showPassword");
+  let $mailInput = $("#E-Mail");
+  let $login = $("#LoginButton");
+
+  $hide.on("click", function() {
+    $(this).hide();
+    $unhide.show();
+    $pwInput.attr("type", "text"); //Inhalt zeigen
+  });
+
+  // Passwort verbergen
+  $unhide.on("click", function() {
+    $(this).hide();
+    $hide.show();
+    $pwInput.attr("type", "password"); //Inhalt verbergen
+  });
+
+  // Login Validierung
+  $login.on("click", function() {
+    if ($mailInput.val() === "" || $pwInput.val() === "") { //wenn ein Feld oder beide Felder leer sind
+      alert("Bitte fülle alle Felder aus");
+    } else {
+      alert("Du hast dich erfolgreich eingeloggt");
+    }
+  });
